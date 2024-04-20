68a575831030d47c529a89ebe38d10e473b0f6c4
Daniel Ort <md.ort@gmx.de>
2024-04-20 12:35:54 +0200

    feat: improved structure of map component, importing map sources

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,11 +1,11 @@
 import React, { FC , useEffect} from 'react';
 import { MapContainer, Marker, Popup, TileLayer, LayersControl, LayerGroup, useMap} from 'react-leaflet';
 import L from 'leaflet';
-import { waypoints } from '../../App'; // import Mock Data
 import MarkerClusterGroup from "react-leaflet-cluster";
 import "leaflet/dist/leaflet.css";
 import { DateTime } from "luxon";
 import { Asset } from '../../models/types';
+import { mapLayers } from './MapSources';
 
 
 interface MapProps {
@@ -14,7 +14,7 @@ interface MapProps {
 
 const Map: FC<MapProps> = ({currentAssets}) => {
     const startCoords = [51.505, -0.09];
-    //const mapRef = useRef(null); // create a handler that can later be used to manipulate the map data...
+    //const mapRef = useRef(null); // create a handler that can later be used to manipulate the map data... seems deprecated
 
     const MapBoundsAdjuster = ({ assets }) => {
       const map = useMap();
@@ -22,7 +22,7 @@ const Map: FC<MapProps> = ({currentAssets}) => {
       useEffect(() => {
         if (assets.length > 0) {
           const bounds = new L.LatLngBounds(assets.map(asset => asset.coordinates));
-          map.fitBounds(bounds,{ minZoom: 1 } ); //maxZoom: map.getZoom()
+          map.fitBounds(bounds,{ minZoom: 1, padding: [50,50]} ); //maxZoom: map.getZoom()
         }
       }, [assets, map]);
     
@@ -40,75 +40,25 @@ const Map: FC<MapProps> = ({currentAssets}) => {
         maxZoom={19}
         maxBounds={[[-85.06, -180], [85.06, 180]]}
         scrollWheelZoom={true}>
-        
-      {/* COULD BE REFINED BY USING THE SAME LOGIC AS IN THE BACKGROUNDMAP COMPONENT AND ITERATE THROUGH THE OBJECT! ! REFACTOR! */}
 
+        
+        {/* ITERATE THROUGH MAP SOURCES AND CREATE LAYER SELECTOR*/}
         <LayersControl>
-        <LayersControl.BaseLayer checked name="Stadia Outdoor">
-        <TileLayer
-            attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
-        /> 
-        </LayersControl.BaseLayer>
-        
-        <LayersControl.BaseLayer name="ESRI World Terrain">
-        <TileLayer
-          attribution='Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS'
-          url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}' />  
-        </LayersControl.BaseLayer>
-
-        <LayersControl.BaseLayer name="CartoDB Voyager">
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png' />  
-        </LayersControl.BaseLayer>
-
-        <LayersControl.BaseLayer  name="Stadia Alidade Smooth Black">
-        <TileLayer
-            attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
-        /> 
-        </LayersControl.BaseLayer>
-
-        <LayersControl.BaseLayer  name="Stadia Alidade Satellite">
-        <TileLayer
-            attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png'
-        /> 
-        </LayersControl.BaseLayer>
-
-        <LayersControl.BaseLayer name="Stanford's Library Map of London 1886">
-        <TileLayer
-            attribution='&copy; <a href="https://mapwarper.net/maps/55847" target="_blank">Mapwarper</a>'
-            url='https://mapwarper.net/maps/tile/55847/{z}/{x}/{y}.png'
-        /> 
-        </LayersControl.BaseLayer>
-
-          <LayersControl.BaseLayer name="Open Street Map">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-
-          <LayersControl.BaseLayer name="Google Maps">
-            <TileLayer
-              attribution="Google Maps"
-              url="https://www.google.com/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-            />
-          </LayersControl.BaseLayer>
-
-          <LayersControl.BaseLayer name="Google Map Satellite">
-            <LayerGroup>
+          {mapLayers.map((mapLayer) => (
+            <LayersControl.BaseLayer 
+              {...(mapLayer.name === 'Stadia Outdoor' ? { checked: true } : {})}
+              key={mapLayer.name} 
+              name={mapLayer.name}>
               <TileLayer
-                attribution="Google Maps Satellite"
-                url="https://www.google.com/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}"
+                attribution={mapLayer.attribution}
+                url={mapLayer.url}
               />
-              <TileLayer url="https://www.google.com/maps/vt?lyrs=y@189&gl=cn&x={x}&y={y}&z={z}" />
-            </LayerGroup>
-          </LayersControl.BaseLayer>
-        </LayersControl>
-        
+            </LayersControl.BaseLayer>
+          ))}
+      </LayersControl>
+
+        {/* POPULATE THE MAP WITH MARKERS AUTOMATICALLY */}
+
         <MarkerClusterGroup>
                 {currentAssets.map(asset => {
                     // create a custom Marker that displays the actual photo
@@ -137,6 +87,8 @@ const Map: FC<MapProps> = ({currentAssets}) => {
                     );
                 })}
             </MarkerClusterGroup>
+
+            {/* ADJUST MAP BOUNDS TO CURRENT MARKERS INSIDE THE MAP */}
             <MapBoundsAdjuster assets={currentAssets} />
         </MapContainer>
     </>
---
+++ client/src/components/Map/MapSources.ts
@@ -0,0 +1,47 @@
+export const mapLayers = [
+    {
+        name: 'Stadia Outdoor',
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        url: 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
+    },
+    {
+        name: 'ESRI World Terrain', 
+        attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}'   
+    },
+    {
+        name: 'CartoDB Voyager', 
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'   
+    },
+    {
+        name: 'Stadia Alidade Smooth Black',
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+    },
+    {
+        name: 'Stadia Alidade Satellite',
+        attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        url: 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png'
+    },
+    {
+        name: "Stanford's Library Map of London 1886",
+        attribution: '&copy; <a href="https://mapwarper.net/maps/55847" target="blank">Mapwarper</a>',
+        url: 'https://mapwarper.net/maps/tile/55847/{z}/{x}/{y}.png'
+    },
+    {
+        name: 'Open Street Map',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    },
+    {
+        name: 'Google Maps',
+        attribution: 'Google Maps',
+        url: 'https://www.google.com/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}'
+    },
+    {
+        name: 'Google Map Satellite',
+        attribution: 'Google Maps Satellite',
+        url: 'https://www.google.com/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
+    }
+];
